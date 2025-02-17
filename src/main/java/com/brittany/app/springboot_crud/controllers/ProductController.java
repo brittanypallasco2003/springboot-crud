a8ae7147ad778d5d51692e588afc2c4e06136456
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-02-17 17:29:58 -0500

    fix: correct URI construction for new resource in POST endpoint

--- src/main/java/com/brittany/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/brittany/app/springboot_crud/controllers/ProductController.java
@@ -6,23 +6,16 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.brittany.app.springboot_crud.models.Product;
 import com.brittany.app.springboot_crud.services.ProductService;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Servlet;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/products")
@@ -35,8 +28,8 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<Product> getProductList() {
-        return productService.findAllProducts();
+    public ResponseEntity<List<Product>> getProductList() {
+        return ResponseEntity.ok().body(productService.findAllProducts());
     }
 
     @GetMapping("/{id}")
@@ -48,27 +41,29 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        Product newproduct = productService.createProduct(product);
         URI location = ServletUriComponentsBuilder
-        .fromCurrentRequest()
-        .path("/{id}")
-        .buildAndExpand(product.getId())
-        .toUri();
-        return ResponseEntity.created(location).body(productService.createProduct(product));
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newproduct.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(newproduct);
     }
 
     // @PutMapping("/{id}")
-    // public ResponseEntity<?> putProduct(@PathVariable Long id, @RequestBody Product product) {
-        
-    //     return entity;
+    // public ResponseEntity<?> putProduct(@PathVariable Long id, @RequestBody
+    // Product product) {
+
+    // return entity;
     // }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteProduct(@PathVariable Long id){
-        Optional<Product> productOptional =productService.deleteProductById(id);
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.deleteProductById(id);
         if (productOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found with: "+id);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found with: " + id);
     }
 
 }
