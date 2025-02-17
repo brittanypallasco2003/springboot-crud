12e5437d0f0e5a68f79d5cc34b5dcfc6d435d48d
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-02-17 12:43:17 -0500

    feat: add GET endpoint to fetch all products

--- src/main/java/com/brittany/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/brittany/app/springboot_crud/controllers/ProductController.java
@@ -2,9 +2,23 @@ package com.brittany.app.springboot_crud.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brittany.app.springboot_crud.models.Product;
 import com.brittany.app.springboot_crud.services.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -12,7 +26,13 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+    @GetMapping
+    public List<Product> getProductList() {
+        return productService.findAllProducts(); 
+    }
 
+   
+    
     
 
 
