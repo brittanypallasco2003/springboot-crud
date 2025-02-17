06c16b100651ebc2e1e3dbbf94b895064d5ddbca
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-02-17 12:30:24 -0500

    feat: add dependency injection for ProductService in controller

--- src/main/java/com/brittany/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/brittany/app/springboot_crud/controllers/ProductController.java
@@ -1,5 +1,19 @@
 package com.brittany.app.springboot_crud.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brittany.app.springboot_crud.services.ProductService;
+
+@RestController
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    
+
 
 }
