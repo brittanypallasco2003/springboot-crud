e4e22222bf547862a5c68269bd35d32d38d80d6c
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-02-17 11:54:12 -0500

    feat: add products management methods in ProductService

---
+++ src/main/java/com/brittany/app/springboot_crud/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.brittany.app.springboot_crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.brittany.app.springboot_crud.models.Product;
+
+public interface ProductService {
+    List<Product> findAllProducts();
+
+    Optional<Product> findProductById(Long id);
+
+    Product createProduct(Product product);
+
+    Product updateProduct(Product product);
+
+    void deleteProductById(Long id);
+}
