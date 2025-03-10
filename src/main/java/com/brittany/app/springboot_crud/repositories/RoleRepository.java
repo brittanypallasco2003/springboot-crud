ad4fae84bb616318c6983b2eb1601e8d65089ebc
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-03-10 14:02:20 -0500

    feat: define search method to find role by name

--- src/main/java/com/brittany/app/springboot_crud/repositories/RoleRepository.java
+++ src/main/java/com/brittany/app/springboot_crud/repositories/RoleRepository.java
@@ -1,9 +1,12 @@
 package com.brittany.app.springboot_crud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.brittany.app.springboot_crud.models.Role;
 
-public interface RoleRepository extends CrudRepository<Role, Long> {
+public interface RoleRepository extends CrudRepository <Role, Long> {
 
+    Optional<Role> findByName(String name);
 }
