323015f0ec3d547b4ece33c4493b0161b4b66722
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-03-10 13:59:26 -0500

    feat: implement user registration logic

--- src/main/java/com/brittany/app/springboot_crud/services/UserServiceImpl.java
+++ src/main/java/com/brittany/app/springboot_crud/services/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.brittany.app.springboot_crud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.brittany.app.springboot_crud.models.Role;
 import com.brittany.app.springboot_crud.models.User;
 import com.brittany.app.springboot_crud.repositories.RoleRepository;
 import com.brittany.app.springboot_crud.repositories.UserRepository;
@@ -33,6 +36,16 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public User registerUser(User user) {
+        Optional<Role> optionalRoleUser= roleRepository.findByName("ROLE_USER");
+        List<Role> roles= new ArrayList<>();
+        optionalRoleUser.ifPresent(role->roles.add(role));
+
+        if (user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin= roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(role->roles.add(role));
+        }
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
