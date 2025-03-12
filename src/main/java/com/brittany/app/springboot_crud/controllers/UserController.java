cd0519b7197ae30043d7b74009bf17d853ff1ebf
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-03-12 16:26:25 -0500

    feat: add user registration endpoint

--- src/main/java/com/brittany/app/springboot_crud/controllers/UserController.java
+++ src/main/java/com/brittany/app/springboot_crud/controllers/UserController.java
@@ -3,10 +3,26 @@ package com.brittany.app.springboot_crud.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brittany.app.springboot_crud.models.User;
 import com.brittany.app.springboot_crud.services.UserService;
 
+import jakarta.validation.Valid;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -14,5 +30,33 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping
+    public List<User> listAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@Valid @RequestBody User user, BindingResult result) {
+        if (result.hasFieldErrors()) {
+            return validation(result);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUsers(user));
+    }
+    
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
+        user.setAdmin(false);
+        return createUser(user, result);
+    }
+
+     private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+    
 
 }
