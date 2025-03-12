12e7be21a4d2ed30b016a52d13193f7bb2800c95
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-03-12 16:03:04 -0500

    feat: create custom annotation to validate product existence in database

---
+++ src/main/java/com/brittany/app/springboot_crud/validation/ExistsDbValidation.java
@@ -0,0 +1,25 @@
+package com.brittany.app.springboot_crud.validation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.brittany.app.springboot_crud.services.ProductService;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+@Component
+public class ExistsDbValidation implements ConstraintValidator<IsExistsDb, String> {
+
+    @Autowired
+    private ProductService productService;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (productService != null) {
+            return !productService.existsBySku(value);
+        }
+        return true;
+    }
+
+}
