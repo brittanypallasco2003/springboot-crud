74474505e3bb40b8bd92fc2db7b3a2543b044fa2
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-03-27 16:16:09 -0500

    feat: define method to verify if username exists

--- src/main/java/com/brittany/app/springboot_crud/services/UserService.java
+++ src/main/java/com/brittany/app/springboot_crud/services/UserService.java
@@ -6,6 +6,8 @@ import com.brittany.app.springboot_crud.models.User;
 
 public interface UserService {
     List<User> getAllUsers();
+    
     User registerUsers(User user);
 
+    boolean existsByUsername(String username);
 }
