04106cccc18ca341f63243e7aed7151c0c2c5605
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-04-02 16:50:25 -0500

    feat: integrate JwtAuthenticationFilter into security filter chain

--- src/main/java/com/brittany/app/springboot_crud/configurations/SpringSecurityConfig.java
+++ src/main/java/com/brittany/app/springboot_crud/configurations/SpringSecurityConfig.java
@@ -1,20 +1,33 @@
 package com.brittany.app.springboot_crud.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.brittany.app.springboot_crud.configurations.filter.JwtAuthenticationFilter;
+
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
@@ -25,6 +38,7 @@ public class SpringSecurityConfig {
         .permitAll()
         .anyRequest()
         .authenticated())
+        .addFilter(new JwtAuthenticationFilter(authenticationManager()))
         .csrf(config ->config.disable())
         .sessionManagement(management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
