fae6254c28dba9e2b76a9a346fcb8b9fd49d4d2d
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-04-09 20:23:20 -0500

    feat: convert unidirectional relationship to bidirectional

--- src/main/java/com/brittany/app/springboot_crud/models/User.java
+++ src/main/java/com/brittany/app/springboot_crud/models/User.java
@@ -1,6 +1,12 @@
 package com.brittany.app.springboot_crud.models;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.brittany.app.springboot_crud.validation.IsExistByUsername;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
@@ -25,26 +32,36 @@ public class User {
 
     @NotBlank
     @Size(min = 4, max = 12)
+    @IsExistByUsername
     @Column(unique = true)
     private String username;
     
     @NotBlank
     @Size(min = 8)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    @JsonIgnoreProperties({"users", "handler", "hibernateLazyInitializer"})
     @ManyToMany
     @JoinTable(
         name = "users_roles",
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name= "role_id"),
-        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id","curso_id"})
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id","role_id"})
     )
     private List<Role> roles;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private boolean enabled;
-
+    
     @Transient
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private boolean isAdmin;
+
+    
+    public User() {
+        this.roles=new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -78,6 +95,7 @@ public class User {
         this.roles = roles;
     }
 
+    @JsonIgnore
     public boolean isAdmin() {
         return isAdmin;
     }
@@ -92,6 +110,42 @@ public class User {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+     @PrePersist
+    public void prePersist(){
+        this.enabled=true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
     }
 
 }
