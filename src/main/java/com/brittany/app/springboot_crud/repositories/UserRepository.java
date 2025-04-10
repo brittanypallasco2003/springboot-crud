845627e478fbe187bec6988b7da9002b91f2f0ab
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-04-09 20:31:19 -0500

    feat: add method to find user by username

--- src/main/java/com/brittany/app/springboot_crud/repositories/UserRepository.java
+++ src/main/java/com/brittany/app/springboot_crud/repositories/UserRepository.java
@@ -3,8 +3,12 @@ package com.brittany.app.springboot_crud.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.brittany.app.springboot_crud.models.User;
+import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<User,Long> {
 
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
